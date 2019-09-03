d631793cb297db25e164553ef1af0dae21cffc97
EyadMohammedOsama <eyadbere@gmail.com>
2019-09-03 20:13:00 +0300

    Initial commit

---
+++ src/tests/symmetric_difference.js
@@ -0,0 +1,18 @@
+const EnhancedArray = require("../EnhancedArray.js");
+
+
+function SymmetricDifferenceTest() {
+    console.log("===== Start of symmetric difference tests =====");
+
+    let arr = new EnhancedArray(2, 4, 7, 11, 13, 16, 19);
+    console.log("Our array is: " + arr);
+    let another_array = [1, 2, 4, 11, 13, 17];
+    console.log("Array to symmertic differentiate with is: " + another_array);
+    let symmetric_difference = arr.difference(another_array);
+    console.log("symmetric difference array is: " + symmetric_difference);
+    
+    console.log("===== End of symmetric difference tests =====");
+    console.log("\n\n\n");
+}
+
+module.exports = SymmetricDifferenceTest;
